d3b05cbed69229f580126dfb2bff9e6db7e3f11d
Mariano Roberto Gomez Omil <mromil@latam.stefanini.com>
2023-09-14 16:27:26 -0300

    DSA

--- Cobradores 2023/TEST/Cobradores_v2/Cobradores_v1/Capa_Datos/Conexion.vb
+++ Cobradores 2023/TEST/Cobradores_v2/Cobradores_v1/Capa_Datos/Conexion.vb
@@ -14,20 +14,20 @@ Public Class Conexion
 
     '-----------------------PARA SERVIDOR DEL CLIENTE------------------------------------------------------------------------------------
     ''SERVIDOR con WinNT
-    'Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=WebCentral;Data Source=localhost")
-    'Public dbconnMaster As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=WebCentral_copy;Data Source=localhost")
+    'Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Cobradores;Data Source=localhost")
+    'Public dbconnMaster As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Cobradores;Data Source=localhost")
 
     '---------------------------------------HAMERSOFT DESARROLLO--------------------------------------------------------------------------
     'bd WEB-Cobradores CHOCO-RYZEN -.local 18-05-2023
-    Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Password=123choco;Persist Security Info=True;User ID=CHOCO;Initial Catalog=Cobradores;Data Source=DESKTOP-IPJ62B9\SQLEXPRESS_CHOK")
+    'Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Password=123choco;Persist Security Info=True;User ID=CHOCO;Initial Catalog=Cobradores;Data Source=DESKTOP-IPJ62B9\SQLEXPRESS_CHOK")
 
     'cadena MASTER
-    Public dbconnMaster As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Password=123choco;Persist Security Info=True;User ID=choco;Initial Catalog=WebCentral_copy;Data Source=DESKTOP-IPJ62B9\SQLEXPRESS_CHOK")
+    ' Public dbconnMaster As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Password=123choco;Persist Security Info=True;User ID=choco;Initial Catalog=WebCentral_copy;Data Source=DESKTOP-IPJ62B9\SQLEXPRESS_CHOK")
 
     'NOTE Mariano Dell
 
-    'Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=WebCentral;Data Source=STFARGAZ203-L\SQLEXPRESS")
-    'Public dbconnMaster As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=WebCentral_copy;Data Source=STFARGAZ203-L\SQLEXPRESS")
+    Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Cobradores;Data Source=STFARGAZ203-L\SQLEXPRESS")
+    Public dbconnMaster As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Cobradores;Data Source=STFARGAZ203-L\SQLEXPRESS")
 
     '-------------------------------------------------------------------------------------------------------------------------------------
 
